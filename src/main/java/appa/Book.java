56db56b5cd7ebe274d842d5075c9d2ada74166f8
Yared Tseghu <jaredtse@hotmail.com>
2016-07-27 18:33:06 +0100

    Additional anotation to get list of types

--- src/main/java/appa/Book.java
+++ src/main/java/appa/Book.java
@@ -1,5 +1,6 @@
 package appa;
 
+import java.lang.annotation.Annotation;
 import java.util.Arrays;
 
 @Author(name = "Raoul")
@@ -8,10 +9,16 @@ import java.util.Arrays;
 @TestAnnotation(surname = "Alan")
 public class Book {
 
+    interface Foo{
+        boolean test();
+    }
+
     public static void main(String[] args) {
 
         Author[] authors = Book.class.getAnnotationsByType(Author.class);
         TestAnnotation[] test = Book.class.getAnnotationsByType(TestAnnotation.class);
+        Annotation[] test2 = Book.class.getDeclaredAnnotations();
+        System.out.println( test2.getClass());
 
         Arrays.asList(test).stream()
                 .forEach( (a) -> {
@@ -23,7 +30,6 @@ public class Book {
         Arrays.asList(authors).stream().forEach(a -> {
             System.out.println(a.name());
         });
-
 
     }
 
